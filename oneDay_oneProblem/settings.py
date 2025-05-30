a5d7a741202910605af1babf966d70cd2fd5f253
daniel0406070 <daniel0406070@gmail.com>
2025-05-30 16:41:18 +0900

    Add User CRUD - add user model - user/login/ - user/register/ - user/delete/ - user/update/

--- oneDay_oneProblem/settings.py
+++ oneDay_oneProblem/settings.py
@@ -45,13 +45,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_apscheduler',
+    'rest_framework',
+    'routine',
+    'users',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
